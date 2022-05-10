637266b9785e71a49491e09a190265622d6d0f5a
Nikolas North <nikolasn09@gmail.com>
2022-05-10 15:07:53 -0400

    connect frontend to backend shipments api

--- repo/api/shipments.ts
+++ repo/api/shipments.ts
@@ -2,9 +2,16 @@ import {Item, Shipment} from "@lib/types";
 
 // Queries API to create a new shipment of the given items. Returns null if the request was unsuccessful.
 export async function createShipment(items: Item[]): Promise<Shipment | null> {
-  const min = 1;
-  const max = 1000;
-  // Random number between [min, max)
-  const id = Math.floor(Math.random() * (max - min) + min);
-  return {id: id, items: items};
+  const res = await fetch("http://localhost:3000/api/shipments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({items: items}),
+  });
+  if (!res.ok) {
+    return null;
+  }
+  const {shipment} = await res.json();
+  return shipment;
 }
